55391bc1bc4f8191a746426d57c289cbeaaacbf6
lateefat <LateefatOba@gmail.com>
2021-07-09 07:07:57 +0100

    firt upload from local

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+document.getElementById("lowercount").addEventListener("click", lowercount);
+document.getElementById("addcount").addEventListener("click", addcount);
+
+
+function lowercount() {
+    let lower = document.getElementById("count").innerHTML;
+    let lower1 = parseInt(lower);
+    if(lower1 > 0) {
+      let lower2 = lower1 - 1;
+       document.getElementById("count").innerHTML = lower2;
+    }
+    
+}
+
+function addcount() {
+    let higher = document.getElementById("count").innerHTML;
+    let higher1 = parseInt(higher);
+    if(higher1 >= 0) {
+      let higher2 = higher1 + 1;
+       document.getElementById("count").innerHTML = higher2;
+       if(higher2 >= 50) {
+        document.getElementById("count").innerHTML = 50;
+
+       }
+    }  
+}
